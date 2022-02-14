9953b4cd2819dfa33221afd35a11a23711c3da9e
dhruvi1996 <ddm8@njit.edu>
2022-02-14 18:16:42 -0500

    add word hello

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Dhruvi Makadia"
+        name = "Hello Dhruvi Makadia"
         return render_template('index.html', name=name)
